7f6b52e6a29d4f7bf37b445b3bc8992aeb5bdbc3
jcromerohdz <jcromerohdz@gmail.com>
2020-01-11 07:44:05 -0800

    JS objectes structures

---
+++ objects/app.js
@@ -0,0 +1,27 @@
+const fitBitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutThisWeek: '5 of 7',
+    avgGoodSleep: '2:13'
+}
+console.log(fitBitData);
+console.log(fitBitData.totalSteps);
+console.log(fitBitData.totalMiles);
+console.log(fitBitData.avgCalorieBurn);
+console.log(fitBitData.workoutThisWeek);
+console.log(fitBitData.avgGoodSleep);
+
+const numbers = {
+    100: 'one hundred',
+    16: 'sixteen'
+}
+
+console.log(numbers[100]);
+console.log(numbers[16]);
+
+const num = {
+    100: 'one hundred',
+    16: 'sixteen'
+}
+
