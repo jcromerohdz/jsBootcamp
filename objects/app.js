bd7932972b701d20301a1e4bd32ae725d8d037a7
Geek Mode <jcromerohdz@gmail.com>
2020-01-12 11:54:57 -0800

    Nesting inside the objects structures and also update and adding items to an object

--- objects/app.js
+++ objects/app.js
@@ -25,3 +25,46 @@ const num = {
     16: 'sixteen'
 }
 
+//Adding and updating properties
+const userReviews = {};
+
+userReviews['queenBee49'] = 4.0;
+userReviews.mrSmith78 = 3.5;
+
+userReviews.queenBee49 += 2;
+console.log(userReviews);
+
+// nested arrays inside of an object
+
+const student = {
+    firstName : 'David',
+    lastName : 'Jones',
+    strengths : ['Music', 'Art'],
+    exams : {
+        midterm : 92,
+        final : 88
+    }
+};
+
+const avg= (student.exams.midterm + student.exams.final) / 2;
+
+console.log(avg);
+
+const shoppingCart = [
+    {
+        product : 'Jenga Classic',
+        price : 6.88,
+        quantity : 1
+    },
+    {
+        product : 'Echo Dont',
+        price : 29.99,
+        quantity : 3
+    },
+    {
+        product : 'Fire Stick',
+        price : 39.99,
+        quantity : 2
+    }
+]
+
