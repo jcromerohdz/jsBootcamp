b46a1d7178447586110bb543ee8919aad040c0a1
jcromerohdz <jcromerohdz@gmail.com>
2020-01-13 11:52:31 -0800

    For nested and while loops

--- loops/app.js
+++ loops/app.js
@@ -1,4 +1,5 @@
 // For loop
+console.log("------------FOR LOOPS--------------")
 for (let num=1; num<=20; num++){
     console.log(`${num}x${num} = ${num * num}`);
 }
@@ -37,7 +38,41 @@ const myStudents = [
     }
 ];
 
+let total = 0;
 for(let i = 0; i < myStudents.length; i++){
     let student = myStudents[i];
     console.log(`${student.firstName} socored ${student.grade}`);
+    total += student.grade;
 }
+console.log(`Average: ${total/myStudents.length}`);
+
+//Nested loops
+
+const gameBoard = [
+    [4, 32, 8, 4],
+    [64, 8, 32, 2],
+    [8, 32, 16, 4],
+    [2, 8, 4, 2]
+];
+
+let totalScore = 0;
+for (let i = 0; i < gameBoard.length; i++){
+    let row = gameBoard[i];
+    for (let j = 0; j < row.length; j++){
+        totalScore += row[j]
+    }
+}
+console.log(`Total Score: ${totalScore}`);
+
+console.log("------------WHILE LOOPS--------------");
+
+const target = Math.floor(Math.random() * 10);
+let guess = Math.floor(Math.random() * 10);
+
+while (guess !== target){
+    console.log(`Target: ${target} Guess: ${guess}`);
+    guess = Math.floor(Math.random() * 10);
+}
+console.log(`Target: ${target} Guess: ${guess}`);
+console.log(`CONGRATS YOU WIN!!`);
+
