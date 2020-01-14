58e43ac9ec4ecf49151645d13d31032321568ea3
J. Christian Romero <jcromerohdz@gmail.com>
2020-01-14 12:59:21 -0800

    arguments in functions

--- functions/app.js
+++ functions/app.js
@@ -1,3 +1,4 @@
+
 function grumpus(){
     console.log('Ugh...you again...');
     console.log('FOR THE LAST TIME...');
@@ -27,3 +28,38 @@ function greet(nickname){
 }
 
 greet('Christian');
+
+function square(num){
+    return num * num;
+}
+
+square(2);
+
+function sum(x, y){
+    return x + y;
+}
+
+sum(5, 5);
+
+function divide(a, b){
+    return a / b;
+}
+
+divide( 4, 1);
+
+function isPurple(color){
+    return color.toLowerCase() === 'purple'; 
+}
+
+console.log(isPurple('Blue'));
+
+function containsPurple(arr){
+    for(let color of arr){
+        if(color === 'purple' || color === 'magenta'){
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(containsPurple(['blue', 'pink', 'magenta']));
