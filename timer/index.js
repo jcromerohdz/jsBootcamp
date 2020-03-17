51e4a9f5bc63b257fd3c477f4bb477b4e8de13a9
jcromerohdz <jcromerohdz@gmail.com>
2020-03-17 09:09:55 -0700

    Finished timer app

--- timer/index.js
+++ timer/index.js
@@ -1,15 +1,22 @@
-
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log('Timer started');
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    console.log('Time jus ticked down');
+  onTick(timeRemaining) {
+    circle.setAttribute('stroke-dashoffset',
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+
   },
   onComplete() {
     console.log('Timer is completed');
--- timer/timer.js
+++ timer/timer.js
@@ -3,7 +3,6 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-
     if (callbacks) {
       this.onStart = callbacks.onStart;
       this.onTick = callbacks.onTick;
@@ -16,11 +15,11 @@ class Timer {
 
   start = () => {
     if (this.onStart) {
-      this.onStart();
+      this.onStart(this.timeRemaining);
     }
     this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  }
+    this.interval = setInterval(this.tick, 50);
+  };
 
   pause = () => {
     clearInterval(this.interval);
@@ -33,12 +32,11 @@ class Timer {
         this.onComplete();
       }
     } else {
-      this.timeRemaining = this.timeRemaining - 1;
+      this.timeRemaining = this.timeRemaining - .05;
       if (this.onTick) {
-        this.onTick();
+        this.onTick(this.timeRemaining);
       }
     }
-
   };
 
   get timeRemaining() {
@@ -46,6 +44,6 @@ class Timer {
   }
 
   set timeRemaining(time) {
-    this.durationInput.value = time
+    this.durationInput.value = time.toFixed(2);
   }
 }
