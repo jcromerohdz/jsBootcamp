b5e2d0b0ba9fd1e666608292a2667c7fa559a8bb
J. Christian Romero <jcromerohdz@gmail.com>
2020-04-02 13:28:30 -0700

    Implementig some initial code with Matter js

---
+++ drawingAnnimations/index.js
@@ -0,0 +1,23 @@
+class Timer {
+	constructor(durationInput, startButton, pauseButton) {
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
+
+		this.startButton.addEventListener('click', this.start);
+	}
+
+	start = () => {
+		this.importantMethodToCall();
+  }
+	
+	importantMethodToCall() {
+		console.log('IMPORTANT THING WAS DONE');
+	}
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
