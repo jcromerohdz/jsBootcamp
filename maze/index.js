9beadc6f600cce2afd07cd9ab0dc52c434b909f8
J. Christian Romero <jcromerohdz@gmail.com>
2020-04-02 20:38:53 -0700

    Performing shuffle function for neighbors

--- maze/index.js
+++ maze/index.js
@@ -32,6 +32,24 @@ const walls = [
 World.add(world, walls);
 
 // Maze generation
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+  console.log(counter)
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  
+  return arr;
+}
+
 const grid = Array(cells)
   .fill(null)
   .map(() => Array(cells).fill(false));
@@ -49,12 +67,27 @@ const startColumn = Math.floor(Math.random() * cells);
 
 const stepThroughCell = (row, column) => {
   // If I have visited the cell at [row, column], then return
-
+  if (grid[row][column]) {
+    return;
+  }
   // Mark this cell ad being visited
-
+  grid[row][column] = true;
   // Assemble randomly-ordered list the neighbors
-
+  const neighbors = shuffle([
+    [row - 1, column],
+    [row, column + 1],
+    [row + 1, column],
+    [row, column - 1]
+  ]);
+  console.log(neighbors);
   // See if that neighbor is out of bounds
+
+  // If we have visited that neighbor, continue to next neighbor
+
+  // Remove a wall from either horizontals or verticals
+
+  // Visit than next cell
 }
 
-console.log(startRow, startColumn);
+stepThroughCell(1, 1);
+
