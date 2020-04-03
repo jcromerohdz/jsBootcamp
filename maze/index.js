75383fb0546f0522fd675be8274a99cf541d126f
J. Christian Romero <jcromerohdz@gmail.com>
2020-04-02 20:15:07 -0700

    Starting the maze algorithm

--- maze/index.js
+++ maze/index.js
@@ -1,20 +1,60 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
+const width = 600;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: false,
+    width: width,
+    height: height
   }
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
-World.add(world, shape);
+
+
+// Walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+
+];
+World.add(world, walls);
+
+// Maze generation
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  // If I have visited the cell at [row, column], then return
+
+  // Mark this cell ad being visited
+
+  // Assemble randomly-ordered list the neighbors
+
+  // See if that neighbor is out of bounds
+}
+
+console.log(startRow, startColumn);
